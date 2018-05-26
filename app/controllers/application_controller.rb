13ca19c8793c9a1953a814cd63d19b15e62bee24
Brandon Mills <sllimnodnarb@gmail.com>
2018-05-25 19:34:14 -0500

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 end
 
 def contact
   ContactMailer.contact.deliver_now
   flash[:alert] = "Thank you for contacting us."
-  redirect_to welcome_index_path
+  redirect_to root_path
 end
